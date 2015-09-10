96ea1a343d0332048d5c0123b3f4a15f3cf1dea8
Bili Xu <xubili93@gmail.com>
2015-09-10 21:31:11 +0800

    Modify header add button href when hash fragment changes

--- public/src/index.js
+++ public/src/index.js
@@ -8,33 +8,10 @@ var LagerApp = React.createClass({
 
   componentDidMount: function() {
     window.onhashchange = function() {
-      this.setState({page: window.location.hash.substring(1)});
+      var hash = window.location.hash.substring(1);
+      $('header a.pull-right').attr('href', '/' + hash + '/new');
+      this.setState({page: hash});
     }.bind(this);
-
-    var socket, host;
-    host = "ws://localhost:4001";
-
-    var connect = function() {
-      try {
-        socket = new WebSocket(host);
-        console.log("Socket State: " + socket.readyState);
-        socket.onopen = function() {
-          console.log("Socket Status: " + socket.readyState + " (open)");
-        }
-        socket.onclose = function() {
-          console.log("Socket Status: " + socket.readyState + " (closed)");
-        }
-        socket.onmessage = function(msg) {
-          var result = JSON.parse(msg.data);
-          React.findDOMNode(this.refs.line).innerHTML =
-            "Host: " + result['host'] + "; Message: " + result['msg'];
-        }.bind(this)
-      } catch(exception) {
-        console.log("Error: " + exception);
-      }
-    }.bind(this)
-
-    connect();
   },
 
   getInitialState: function() {
