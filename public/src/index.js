af0b6f9a588e4e0f1640383a52432c1221dda9c0
Jingwen Chen <jin@crypt.sg>
2015-09-14 02:24:57 +0800

    Show server count for demo purposes

--- public/src/index.js
+++ public/src/index.js
@@ -100,7 +100,7 @@ var ServiceTableViewCell = React.createClass({
         <a className="navigate-right" data-ignore="push" href={"/logs/service/" + this.props.service.id}>
           <div>
             <h4>{this.props.service.name}</h4>
-             <p>Server count: {this.props.service.servers.length}</p>
+             <p>Server count: {this.props.service.server_count}</p>
           </div>
         </a>
       </li>
