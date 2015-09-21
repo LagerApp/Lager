9ce52dc5f06fe5ac5c670c787b0dbd1f9327b55d
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-09-21 22:16:41 +0800

    added the message to console, which had been removed by mistake

--- lib/log_helper.rb
+++ lib/log_helper.rb
@@ -15,6 +15,7 @@ end
 
 def stream_log(host, logPath, &block)
   connect_to_host(host) do |ch|
+    puts "Tailing log file: #{logPath}"
     ch.exec "tail -n 200 -f #{logPath}" do |ch, success|
       yield(ch, success)
     end
