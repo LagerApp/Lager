e29844f648c2562b236192860d2118c28cc4179a
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-09-21 15:32:02 +0800

    Modified to remove the timestamp once parsed from the original log data

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,21 +2,25 @@ require 'rubygems'
 
 def parse_time(data)
 	mysql_time_format = "[0-9]{2}:[0-9]{2}:[0-9]{2}"
-	clf_time_format = "[0-9]+\/[a-zA-Z]{3}\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2}"
+	clf_time_format = "[0-9]+\/[a-zA-Z]{3}\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} [+-][0-9]{4}"
 	apache_time_format = "[a-zA-Z]{3} [a-zA-Z]{3} [0-9]+ [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}"
 
 	if /#{clf_time_format}/ =~ data
 		str = data.match(/#{clf_time_format}/).to_s
-		time = DateTime.strptime( str, "%d/%b/%Y:%H:%M:%S")	
+		time = DateTime.strptime( str, "%d/%b/%Y:%H:%M:%S %z")	
+		data[/\[#{str}\]/] = ''
 	elsif /#{apache_time_format}/ =~ data
 		str = data.match(/#{apache_time_format}/).to_s
-		time = DateTime.strptime( str, "%b %d %H:%M:%S %Y")		
+		time = DateTime.strptime( str, "%a %b %d %H:%M:%S %Y")		
+		data[/\[#{str}\]/] = ''
 	elsif /#{mysql_time_format}/ =~ data
 		str = data.match(/#{mysql_time_format}/).to_s
 		time = DateTime.strptime( str, "%H:%M:%S")						
+		data[/#{str}/] = ''
 	else 
 		time = nil		
 	end
 
-	return time
+	parsed_data = Hash["data" => data, "timestamp" => time]
+	return parsed_data
 end
